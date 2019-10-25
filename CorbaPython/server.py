84bb53c8dbca6f666104c4dbd644a8d45235bae6
André Luiz Bezerra Costa <34591259+AndreOSf@users.noreply.github.com>
2019-10-25 11:49:07 -0300

    update server

--- CorbaPython/server.py
+++ CorbaPython/server.py
@@ -6,19 +6,14 @@ class CookieServer_i(Fortune__POA.CookieServer):
         msg = "Testando comunicação com OmniORBpy\n"
         return msg
 
-#inicia o orb do corba
 orb = CORBA.ORB_init(sys.argv)
-#POA raiz servidor -> cliente
 poa = orb.resolve_initial_references("RootPOA")
 
-#instancia o server
 servant = CookieServer_i()
 #ativa o objeto do serve com a poa raiz
 poa.activate_object(servant)
-
-#print na tela
 print orb.object_to_string(servant._this())
 
-#Referencia a POA ativa
+#POA ativa
 poa._get_the_POAManager().activate()
 orb.run()
