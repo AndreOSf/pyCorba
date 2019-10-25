85946c3faa5da95214b74be1301993df132c6a60
André Luiz Bezerra Costa <34591259+AndreOSf@users.noreply.github.com>
2019-10-25 11:50:13 -0300

    ajuste no client

--- CorbaPython/client.py
+++ CorbaPython/client.py
@@ -2,7 +2,8 @@ import CORBA, Fortune
 
 orb = CORBA.ORB_init()
 print "Insira o IOR:"
-testVar = raw_input()
-o = orb.string_to_object(testVar)
+
+iorVar = raw_input()
+o = orb.string_to_object(iorVar)
 o = o._narrow(Fortune.CookieServer)
 print o.get_cookie()
